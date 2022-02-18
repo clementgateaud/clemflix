964be64d76159f87d8e2d56f59a7e1d045b02240
Clement Gateaud <clement.gateaud@gmail.com>
2022-02-18 14:10:32 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+require "json"
+require "open-uri"
 
 Bookmark.destroy_all
 Movie.destroy_all
-List.destroy_all
+# List.destroy_all
 
 # List.create(name: "Action")
 # List.create(name: "Comedy")
@@ -18,12 +20,18 @@ List.destroy_all
 # List.create(name: "Adventure")
 # List.create(name: "Romance")
 
-100.times do
-  Movie.create(title: Faker::Movie.title, overview: Faker::Lorem.paragraph(sentence_count: 10), poster_url: "https://picsum.photos/500/300", rating: rand(5.0..10.0).round(1))
+
+movies = JSON.parse(URI.open("https://api.themoviedb.org/3/movie/popular?api_key=62a3626345c7fcb86e890d20ca9de2a3").read)
+
+movies["results"].each_with_index do |movie|
+  title = movie["original_title"]
+  overview = movie["overview"]
+  poster_url = "https://image.tmdb.org/t/p/original#{movie["poster_path"]}"
+  rating = movie["vote_average"]
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
 end
+
 
 # 100.times do
 #   Bookmark.create(list: List.all.sample, movie: Movie.all.sample, comment:Faker::Lorem.sentence)
 # end
-
-#/movie/popular
