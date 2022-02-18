ce3f56fe347b6ad2d3140aa5cdb330aa6251f428
Clement Gateaud <clement.gateaud@gmail.com>
2022-02-18 16:45:14 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 require "json"
 require "open-uri"
 
 Bookmark.destroy_all
 Movie.destroy_all
-# List.destroy_all
+List.destroy_all
 
-# List.create(name: "Action")
-# List.create(name: "Comedy")
-# List.create(name: "Cartoon")
-# List.create(name: "Adventure")
-# List.create(name: "Romance")
-
+file = URI.open('https://fr.web.img2.acsta.net/newsv7/20/03/16/12/03/3134680.jpg')
+list = List.create(name: "My first list")
+list.photo.attach(io: file, filename: 'myfirstlist.png', content_type: 'image/png')
 
 movies = JSON.parse(URI.open("https://api.themoviedb.org/3/movie/popular?api_key=62a3626345c7fcb86e890d20ca9de2a3").read)
 
@@ -31,7 +20,6 @@ movies["results"].each_with_index do |movie|
   Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
 end
 
-
-# 100.times do
-#   Bookmark.create(list: List.all.sample, movie: Movie.all.sample, comment:Faker::Lorem.sentence)
-# end
+10.times do
+  Bookmark.create(list: list, movie: Movie.all.sample, comment:"I loved that movie")
+end
