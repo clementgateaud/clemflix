ad13460bc0acf926f21c89cf82132bd2dfa75381
Clement Gateaud <clement.gateaud@gmail.com>
2022-02-18 17:57:49 +0100

    done

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_one_attached :photo
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
