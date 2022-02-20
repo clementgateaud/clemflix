f02ca7573222b1e73cca3d8ed967bf20f30b9d4d
Clement Gateaud <clement.gateaud@gmail.com>
2022-02-20 17:33:20 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ list.photo.attach(io: file, filename: 'myfirstlist.png', content_type: 'image/pn
 
 movies = JSON.parse(URI.open("https://api.themoviedb.org/3/movie/popular?api_key=62a3626345c7fcb86e890d20ca9de2a3").read)
 
-movies["results"].each_with_index do |movie|
+movies["results"].first(5).each_with_index do |movie|
   title = movie["original_title"]
   overview = movie["overview"]
   poster_url = "https://image.tmdb.org/t/p/original#{movie["backdrop_path"]}"
